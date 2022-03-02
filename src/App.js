e2c8e102047c7a3e20f70f6934dbfa9e3531d09f
Christina Izquierdo <83507941+christina-izquierdo@users.noreply.github.com>
2022-03-02 12:10:25 +0100

    added a search form and the keyword useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <footer>
-      <a href='https://www.freepik.com/vectors/background'>Background vector created by rawpixel.com - www.freepik.com</a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a href='https://www.freepik.com/vectors/background'>Background vector created by rawpixel.com - www.freepik.com</a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
