2c9115122da416a2c3c55b2825bceab7b0685211
Christina Izquierdo <83507941+christina-izquierdo@users.noreply.github.com>
2022-03-03 12:46:56 +0100

    updated the colors and css of all headings and footer

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,34 +4,44 @@ import Results from "./Results.js";
 import "./Dictionary.css";
 
 export default function Dictionary(){
-    let [keyword, setKeyword] = useState("");
+    let [keyword, setKeyword] = useState("sunset");
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
     function handleResponse(response){
         setResults(response.data[0]);
     }
-    function search(event){
+    function search(){
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+    function handleSubmit(event){
        event.preventDefault();
-       
-       
-       let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-       axios.get(apiUrl).then(handleResponse);
+        search();
     }
 
     function handleKeywordChange(event){
         setKeyword(event.target.value);
+    } 
+    function load(){
+        setLoaded(true);
+        search();
     }
-    return (
+    if (loaded){
+        return (
         <div className="Dictionary">
             <section>
-                <form onSubmit={search}>
+                <form onSubmit={handleSubmit}>
                     <input type="search" onChange={handleKeywordChange}/>
                 </form>
                 <div className="hint">
-                suggested words: retro, computer, waves, cats
+                suggested words: retro, computer, waves, cat
             </div>
             </section>
            
             <Results results={results}/>
         </div>
-    )
-}
+        );
+} else {
+    load();
+    return "Loading";
+}}
